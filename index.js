66521515a630cc217a29188857e9001082bc3191
QuantumCoded <bfields3@jscc.edu>
2022-02-27 20:02:10 -0600

    add user agent

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const https = require('https');
+const http = require('http');
 
-https.createServer((req, res) => {
+
+http.createServer((req, res) => {
     https.request('https://quizlet.com/106508204/testout-practice-questions234-4103-flash-cards/', req_res => {
-        req_res.pipe(res).writeHead(200, 'OK').end();
-    })
+        req_res.pipe(res.writeHead(200, 'OK'));
+    }).setHeader('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0').end();
 })
-.listen(process.env.PORT || 8080);
+.listen(process.env.PORT || 8008);
